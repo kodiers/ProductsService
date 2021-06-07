67dcd69d639b21a5eab279cb1864875524191d69
Victor Yamchinov <kodiers@gmail.com>
2021-06-08 01:52:07 +0300

    added axon framework and product model/command

--- src/main/java/com/tfl/estore/productsservice/rest/ProductsController.java
+++ src/main/java/com/tfl/estore/productsservice/rest/ProductsController.java
@@ -1,14 +1,25 @@
 package com.tfl.estore.productsservice.rest;
 
+import com.tfl.estore.productsservice.command.CreateProductCommand;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
 
     @PostMapping
-    public String createProduct() {
-        return "HTTP POST handled";
+    public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
+
+        CreateProductCommand createProductCommand = CreateProductCommand.builder()
+                .price(createProductRestModel.getPrice())
+                .quantity(createProductRestModel.getQuantity())
+                .title(createProductRestModel.getTitle())
+                .productId(UUID.randomUUID().toString())
+                .build();
+
+        return "HTTP POST handled" + createProductRestModel.getTitle();
     }
 
     @GetMapping
