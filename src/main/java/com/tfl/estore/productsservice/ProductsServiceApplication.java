888bbe4c4912d00d1e88e1be314d64b087607882
Victor Yamchinov <kodiers@gmail.com>
2021-06-30 20:44:20 +0300

    reject snapshotter

--- src/main/java/com/tfl/estore/productsservice/ProductsServiceApplication.java
+++ src/main/java/com/tfl/estore/productsservice/ProductsServiceApplication.java
@@ -4,11 +4,15 @@ import com.tfl.estore.productsservice.command.interceptors.CreateProductCommandI
 import com.tfl.estore.productsservice.core.errorhandling.ProductsServiceEventsErrorHandler;
 import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventsourcing.EventCountSnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.Snapshotter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -27,6 +31,11 @@ public class ProductsServiceApplication {
     public void configure(EventProcessingConfigurer configurer) {
         configurer.registerListenerInvocationErrorHandler("product-group",
                 configuration -> new ProductsServiceEventsErrorHandler());
+    }
+
+    @Bean(name = "productSnapshotTriggerDefinition")
+    public SnapshotTriggerDefinition productSnapshotTriggerDefinition(Snapshotter snapshotter) {
+        return new EventCountSnapshotTriggerDefinition(snapshotter, 3);
     }
 
 }
